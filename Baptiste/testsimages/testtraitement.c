e2c583a538d2e73a483fffe757d81905e3b0b262
Baptiste PREVOT <baptiste.prevot@epita.fr>
2021-10-07 15:09:42 +0200

    Martin test

---
+++ Baptiste/testsimages/testtraitement.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <SDL/SDL_image.h>
+
+int main (int argc, char *argv[])
+{
+    SDL_Surface *ecran = NULL, *imageDeFond = NULL;
+    SDL_Rect positionFond;
+
+    positionFond.x = 0;
+    positionFond.y = 0;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    ecran = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE);
+    SDL_WM_SetCaption("Chargement d'images en SDL", NULL);
+
+    /* Chargement d'une image Bitmap dans une surface */
+    imageDeFond = SDL_LoadBMP("lac_en_montagne.bmp");
+    /* On blitte par-dessus l'écran */
+    SDL_BlitSurface(imageDeFond, NULL, ecran, &positionFond);
+
+    SDL_Flip(ecran);
+    pause();
+
+    SDL_FreeSurface(imageDeFond); /* On libère la surface */
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
