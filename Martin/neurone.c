f8387e7bd9cf5edc217682c52331f438ddde8851
chuklee <martin2.natale@epita.fr>
2021-10-13 09:08:47 +0200

    pb git antoine

--- Martin/neurone.c
+++ Martin/neurone.c
@@ -7,9 +7,14 @@ int initialize(){
 		{1,0},
 		{1,1}
 	};
-for(int i=0; i<4;i++){
-	printf("[%d,%d]", output[i][0], output[i][1]);
-}
-	return 0;
+    for(int i=0; i<4;i++){
+	    printf("[%d,%d]", output[i][0], output[i][1]);
+    }
+    float expected_output[4] = {0.0, 1.0, 1.0, 0.0};
+    for(int i=0; i<4;i++){
+        printf("[%.6f]", expected_output[i]);
+    }
+    int bias[][]
+return 0;
 }
 
