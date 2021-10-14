cf24b94f798552de74cc4982b58d3aa7c58d134d
Martin Natale <martin2.natale@epita.fr>
2021-10-14 22:29:09 +0200

    Comprehension IA

--- Martin/neurone.c
+++ Martin/neurone.c
@@ -17,6 +17,17 @@ double initialize(){
 		{1,0},
 		{1,1}
 	};
+	int expected_output[4] = {0,1,1,0};
+	int inputLayersNeurons = 2;
+	int hiddenLayerNeurons = 2;
+	int outpuyLayerNeurons = 1;
+
+	int hiddeneye[2][2] = {
+		{/*aleatoire 01,01*/},
+		{/*aleatoire 01,01*/}
+	}
+
+
 	    /*printf("[%d,%d]", output[i][0], output[i][1]);*/
     
     	printf("\n");
