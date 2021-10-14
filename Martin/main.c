829eb76779f2c564166ee4ead71d151dac03c240
Martin Natale <martin2.natale@epita.fr>
2021-10-14 13:49:40 +0200

    RAS

--- Martin/main.c
+++ Martin/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include "neurone.h"
 
 int main(){
--- Martin/neurone.c
+++ Martin/neurone.c
@@ -4,19 +4,12 @@
 #include <math.h>
 
 
-double exp(double value){
-
-
 double sigmoid(double value){
-    	double result;
-
-    	/* Assign the value we will find the exp of */
-    	
-
-    	/* Calculate the exponential of the value */
-    	result = exp(value);
-	return result;
+    	return 1 / exp(-value);
 }
+
+
+
 double initialize(){
 	int output[4][2] = {
 		{0,0},
@@ -24,13 +17,8 @@ double initialize(){
 		{1,0},
 		{1,1}
 	};
-    for(int i=0; i<4;i++){
-	    printf("[%d,%d]", output[i][0], output[i][1]);
-    }
-    float expected_output[4] = {0.0, 1.0, 1.0, 0.0};
-    for(int i=0; i<4;i++){
-        printf("[%.6f]", expected_output[i]);
-    }
+	    /*printf("[%d,%d]", output[i][0], output[i][1]);*/
+    
     	printf("\n");
 	printf("%f", sigmoid(1));
 
