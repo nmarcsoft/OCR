235feab79d07774ed1f087f455bea65f32a5e05d
Antoine Ollivier <antoine.ollivier@epita.fr>
2021-10-18 18:45:31 -0400

    Way Sudoku -> Text done

--- Antoine/main.c
+++ Antoine/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "solving.h"
 
 
@@ -15,24 +16,78 @@ void print(int arr[N][N])
 }
 
 
+void print_in_txt(int arr[N][N])
+{
+	FILE *fptr;
+	fptr = fopen("/home/antoll/Desktop/EPITA/PROJET/OCR/Antoine/grid_00.result","w");
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			//arr[i][j]
+			if(j % 3 == 0)
+			{
+				fputs(" ",fptr);
+				fprintf(fptr,"%d",arr[i][j]);
+			}
+			if(i % 3 == 0)
+			{
+				fputs(" ",fptr);
+				fprintf(fptr,"%d",arr[i][j]);
+			}
+			else{
+			fprintf(fptr,"%d",arr[i][j]);
+			}
+		}
+		fputs("\n", fptr);
+	}
+}
+
+void print_in_txt_v2(int arr[N][N])
+{
+	FILE *fptr;
+	fptr = fopen("grid_00.result","w");
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < (N/3); j++)
+		{
+			fprintf(fptr, "%d",arr[i][j]);
+		}
+		fputs(" ", fptr);
+		for(int j = 3; j < (N/3)*2; j++)
+		{
+			fprintf(fptr,"%d", arr[i][j]);
+		}
+		fputs(" ", fptr);
+		for(int j = 6; j < N; j++)
+		{
+			fprintf(fptr,"%d" ,arr[i][j]);
+		}
+		fputs("\n",fptr);
+		if((i == 2) | (i == 5))
+		{
+			fputs("\n", fptr);
+		}
+	}
+}	
+
+
+	
+
 int main()
 {
-    // 0 means unassigned cells
-    int grid[9][9] = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
-                       { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
-                       { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
-                       { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
-                       { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
-                       { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
-                       { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
-                       { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
-                       { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
+    int grid[9][9] = { { 0, 0, 0, 0, 0, 4, 5, 8, 0 },
+                       { 0, 0, 0, 7, 2, 1, 0, 0, 3 },
+                       { 4, 0, 3, 0, 0, 0, 0, 0, 0 },
+                       { 2, 1, 0, 0, 6, 7, 0, 0, 4 },
+                       { 0, 7, 0, 0, 0, 0, 2, 0, 0 },
+                       { 6, 3, 0, 0, 4, 9, 0, 0, 1 },
+                       { 3, 0, 6, 0, 0, 0, 0, 0, 0 },
+                       { 0, 0, 0, 1, 5, 8, 0, 0, 6 },
+                       { 0, 0, 0, 0, 0, 6, 9, 5, 0 } };
 
-    if (solveSuduko(grid, 0, 0)==1)
-        print(grid);
+    if (solveSudoku(grid, 0, 0)==1)
+        print_in_txt_v2(grid);
     else
         printf("No solution exists");
 
     return 0;
-    // This is code is contributed by Pradeep Mondal P
 }
--- Antoine/solving.c
+++ Antoine/solving.c
@@ -1,104 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
  
-// N is the size of the 2D matrix   N*Ni
+// N is the size of the matrix
 #define N 9
  
- 
-// Checks whether it will be legal 
-// to assign num to the
-// given row, col
+// isSafe will check if it is allowed to put the num
+// at the row,col or 3x3 matrix in param
 int isSafe(int grid[N][N], int row,
                        int col, int num)
 {
      
-    // Check if we find the same num
-    // in the similar row , we return 0
+    // Check if its safe to put the num on this row
+    // return 0 if not
+
     for (int x = 0; x <= 8; x++)
         if (grid[row][x] == num)
             return 0;
  
-    // Check if we find the same num in the
-    // similar column , we return 0
+    // Check if its safe to put the num on thos col
+    // return 0 if not
+
     for (int x = 0; x <= 8; x++)
         if (grid[x][col] == num)
             return 0;
  
-    // Check if we find the same num in the
-    // particular 3*3 matrix, we return 0
-    // startRow and startCol to know win which
-    // particular 3*3 we are
-    int startRow = row - row % 3;
-    int startCol = col - col % 3;
+    // Check if its safe to put the num on this 3x3 matrix
+    // return 0 if not
+    int WhichRow = row - row % 3;
+    int WhichCol = col - col % 3;
    
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
-            if (grid[i + startRow][j +
-                          startCol] == num)
+            if (grid[i + WhichRow][j +
+                          WhichCol] == num)
                 return 0;
-    //return 1 if it's safe to put the x at this case 
+    //return 1 if we can put the num at the case 
     return 1;
 }
- 
-/* Takes a partially filled-in grid and attempts
-to assign values to all unassigned locations in
-such a way to meet the requirements for
-Sudoku solution (non-duplication across rows,
-columns, and boxes) */
-int solveSuduko(int grid[N][N], int row, int col)
+
+//solveSudoku will seek in the grid given, the 
+//different cases that are empty and will try to assign
+//them to a number by respecting the isSafe fucntion
+//for each numbers.
+int solveSudoku(int grid[N][N], int row, int col)
 {
-     
-    // Check if we have reached the 8th row
-    // and 9th column (0
-    // indexed matrix) , we are
-    // returning true to avoid
-    // further backtracking
+    // Check if we have reached the 8th row and the 8th
+    // column
     if (row == N - 1 && col == N)
         return 1;
  
-    //  Check if column value  becomes 9 ,
-    //  we move to next row and
-    //  column start from 0
+    //  Check if we are at the end of the row to go 
+    //  to the other one
     if (col == N)
     {
         row++;
         col = 0;
     }
-   
-    // Check if the current position
-    // of the grid already contains
-    // value >0, we iterate for next column
+    // Check if the grid[row][col] is empty, if yes,
+    // then we move to the next case
     if (grid[row][col] > 0)
-        return solveSuduko(grid, row, col + 1);
+        return solveSudoku(grid, row, col + 1);
  
     for (int num = 1; num <= N; num++)
-    {
-         
-        // Check if it is safe to place
-        // the num (1-9)  in the
-        // given row ,col  ->we move to next column
+    { 
+	// Check if its safe to put the num in the for
+	// in the given grid[row][col]
         if (isSafe(grid, row, col, num)==1)
         {
-            /* assigning the num in the
-               current (row,col)
-               position of the grid
-               and assuming our assigned num
-               in the position
-               is correct     */
+	    // If isSafe return 1, its safe to put the num 
+	    // at this grid[row][col] place, so we put it
             grid[row][col] = num;
            
-            //  Checking for next possibility with next
-            //  column
-            if (solveSuduko(grid, row, col + 1)==1)
+            //  Checking for the next possibility with the
+            //  next column
+            if (solveSudoku(grid, row, col + 1)==1)
                 return 1;
         }
-       
-        // Removing the assigned num ,
-        // since our assumption
-        // was wrong , and we go for next
-        // assumption with
-        // diff num value
         grid[row][col] = 0;
     }
     return 0;
 }
+
