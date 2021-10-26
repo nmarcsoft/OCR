16e6f6ae68fc555a3be78560086ec9e7d08796a0
Antoine Ollivier <antoine.ollivier@epita.fr>
2021-10-25 20:26:25 -0400

    Solver fini à 99%, manque le MakeFile

--- Antoine/main.c
+++ Antoine/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <err.h>
+#include <string.h>
+
 #include "solving.h"
 
 //function that is printing an arr matrix of N*N size
 //in a .txt file called grid_00.result.
 
+/*
 void print_in_txt(int arr[N][N])
 {
 	FILE *fptr;
-	fptr = fopen("grid_00.result","w"); //opening the file
+	fptr = fopen("grid_00.result","w+"); //opening the file
 	for(int i = 0; i < N; i++)
 	{
 		//the first loop is to print in the file the first
@@ -50,39 +54,56 @@ void print_in_txt(int arr[N][N])
 			fputs("\n", fptr);
 		}
 	}
-}
+}*/
+
 	 
 
 
-int main()
+int main(int argc, char *argv[])
 {
+
+
     //initializing a 9x9 matrix full of 0
-   int e[9][9]={{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                       { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                       { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                       { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                       { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                       { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                       { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                       { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                       { 0, 0, 0, 0, 0, 0, 0, 0, 0 }};
+   /*int e[9][9]={{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                  { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                  { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                  { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                  { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                  { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                  { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                  { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                  { 0, 0, 0, 0, 0, 0, 0, 0, 0 }};*/
+
+     
+    int e[9][9] = {0};
 
     
     FILE *fptr;
     
+    if(argc != 2){
+        errx(1,"Need ./solver with the Name of the Grid");
+    }
+
+    int len = strlen(argv[1]);
+    char* FileName = NULL;
+    FileName = malloc((len+7)*sizeof(int));
+    if(FileName == NULL){
+        errx(2,"malloc() doesn't work");
+    }
+    FileName = argv[1];
+
+
     //if the file doesn't exist, then we return an error to the user.
 
-    if((fptr = 
-fopen("/home/antoll/Desktop/EPITA/PROJET/OCR/Antoine/grid_00","r")) 
-		    == NULL){
-	    printf("No such file given called grid_00 is in the repository");
-	    exit(1);
+    if((fptr = fopen(FileName,"r")) == NULL)
+    {
+	    errx(1,"No such file given called in parameter is in the repository");
     }
     char c;
     int i = 0;
     int j = 0;
     int space = 0;
-    int ligne = 0;
+    int line = 0;
     
     //else, while fgetc having a char
     //we are going, in this while to re write the matrix in the .txt file
@@ -112,7 +133,7 @@ fopen("/home/antoll/Desktop/EPITA/PROJET/OCR/Antoine/grid_00","r"))
 			    space +=1;
 			    break;
 		    case(10):
-			    ligne +=1;
+			    line +=1;
 			    break;
 		    default:
 			    e[i][j] = c -48;
@@ -128,7 +149,55 @@ fopen("/home/antoll/Desktop/EPITA/PROJET/OCR/Antoine/grid_00","r"))
     //called grid_00.result
 
     if (solveSudoku(e, 0, 0)==1)
-        print_in_txt(e);
+    {
+        //print_in_txt(e);
+        char res[] = ".result";
+        strcat(FileName,res);
+        
+        FILE *fptr2;
+	    fptr2 = fopen(FileName,"w+"); //opening the file
+	    for(int i = 0; i < N; i++)
+	    {
+		    //the first loop is to print in the file the first
+            //3 numbers of the row.
+
+            for(int j = 0; j < (N/3); j++)
+		    {
+			    fprintf(fptr2, "%d",e[i][j]);
+		    }
+
+		    fputs(" ", fptr2); // there is a space between bloc of 3 numbers
+
+            //the second loop is to print in the file the 4-6 
+            //numbers of the row.
+        
+		    for(int j = 3; j < (N/3)*2; j++)
+		    {
+			    fprintf(fptr2,"%d", e[i][j]);
+		    }
+
+		    fputs(" ", fptr2); //once again, another space
+
+            //the last loop to print in the FILE
+            //the 3 last numbers of the row
+        
+		    for(int j = 6; j < N; j++)
+		    {
+			    fprintf(fptr2,"%d" ,e[i][j]);
+		    }
+
+            //at the end of these loops, we go back to the line 
+            //and if i is equal to 2 or 5, we jump to another line
+            //to be conform with the .txt representation of the solver
+            //and we do it again on the next row.
+        
+		    fputs("\n",fptr2);
+		    if((i == 2) | (i == 5))
+		    {
+			    fputs("\n", fptr2);
+		    }
+	    }
+    }
     else
         printf("No solution exists");
 
